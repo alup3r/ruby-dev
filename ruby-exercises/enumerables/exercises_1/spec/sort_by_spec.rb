9caa5650ada7731ddd628d28b52c0b26b7b4423d
aluper <aluper@lx.lco.net>
2024-11-10 09:05:45 -0500

    completed sort_by_spec exercises

--- ruby-exercises/enumerables/exercises_1/spec/sort_by_spec.rb
+++ ruby-exercises/enumerables/exercises_1/spec/sort_by_spec.rb
@@ -1,42 +1,38 @@
 RSpec.describe 'sort_by' do
 
   it 'alphabetically' do
-    words = ["broccoli", "Carrots", "FISH", "Bacon", "candy"]
-    sorted = words.sort_by do |word|
-      word.downcase
-    end
-    expect(sorted).to eq(["Bacon", "broccoli", "candy", "Carrots", "FISH"])
+    words = %w[broccoli Carrots FISH Bacon candy]
+    sorted = words.sort_by(&:downcase)
+    expect(sorted).to eq(%w[Bacon broccoli candy Carrots FISH])
   end
 
-  xit 'alphabetically by last letter' do
-    things = ["pill", "box", "glass", "water", "sponge"]
-    sorted = things.sort_by do |thing|
-      # Your code goes here
-    end
-    expect(sorted).to eq(["sponge", "pill", "water", "glass", "box"])
+  it 'alphabetically by last letter' do
+    things = %w[pill box glass water sponge]
+    sorted = things.sort_by { |thing| thing[-1] }
+    expect(sorted).to eq(%w[sponge pill water glass box])
   end
 
-  xit 'distance' do
-    distances = ["1cm", "9cm", "30cm", "4cm", "2cm"]
-    # Your code goes here
-    expect(sorted).to eq(["1cm", "2cm", "4cm", "9cm", "30cm"])
+  it 'distance' do
+    distances = %w[1cm 9cm 30cm 4cm 2cm]
+    sorted = distances.sort_by(&:to_i)
+    expect(sorted).to eq(%w[1cm 2cm 4cm 9cm 30cm])
   end
 
-  xit 'length' do
-    words = ["heteromorph", "ancyloceratina", "bioengineering", "mathematical", "bug"]
-    # Your code goes here
-    expect(sorted).to eq(["bug", "heteromorph", "mathematical", "ancyloceratina", "bioengineering"])
+  it 'length' do
+    words = %w[heteromorph ancyloceratina bioengineering mathematical bug]
+    sorted = words.sort_by(&:length)
+    expect(sorted).to eq(%w[bug heteromorph mathematical ancyloceratina bioengineering])
   end
 
-  xit 'proximity to ten' do
+  it 'proximity to ten' do
     prices = [3.02, 9.91, 17.9, 10.01, 11.0]
-    # Your code goes here
+    sorted = prices.sort_by { |price| (price - 10).abs }
     expect(sorted).to eq([10.01, 9.91, 11.0, 3.02, 17.9])
   end
 
-  xit 'number of cents' do
+  it 'number of cents' do
     prices = [3.02, 9.91, 7.9, 10.01, 11.0]
-    # Your code goes here
+    sorted = prices.sort_by { |price| price.to_s.split('.').last }
     expect(sorted).to eq([11.0, 10.01, 3.02, 7.9, 9.91])
   end
 end
